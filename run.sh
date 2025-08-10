3aee164da39dfd910318e3943dda8dd142eb5dc0
yankai <huangyankai@stu.xjtu.edu.cn>
2025-08-10 23:10:18 +0800

    ✨ feat(dapr输入+计时): 完成dapr输入及计时、地址实验
    获取需要记录的运行时间 和 连续访问的连续地址长度

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-python3 -m flexllmgen.impress_opt --model facebook/opt-6.7b \
+python3 -m flexllmgen.dapr_opt --model facebook/opt-30b \
         --percent 100 0 100 0 100 0 \
         --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights  \
-        --offload-dir /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/my_FlexLLMGen/flexllmgen_offload_dir  > test.log 2>&1
+        --prompt-len 2016 \
+        --gen-len 32 \
+        --offload-dir /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/my_FlexLLMGen/flexllmgen_offload_dir  > test18.log 2>&1
