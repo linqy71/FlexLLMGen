f0deeba989ded99482b1154f7e9ef0d350457c72
linqy71 <2575769386@qq.com>
2025-12-07 16:07:03 +0800

    test rte and copa

--- lm_eval_impress/eval_tasks.sh
+++ lm_eval_impress/eval_tasks.sh
@@ -1,88 +1,175 @@
 #!/bin/bash
 
 
-############30b###################
+# # #############6.7b###################
 # MODEL_NAME="facebook/opt-66b"
-MODEL_NAME="facebook/opt-30b"
+MODEL_NAME="facebook/opt-6.7b"
 MODEL_TYPE="opt"
 
+# for TASK_NAME in "openbookqa" "copa" "rte"
 for TASK_NAME in "rte"
 do
 
-INPUT_FILE="datasets/${TASK_NAME}_fs10.jsonl"
-for IMP_R in 1.0
+INPUT_FILE="datasets/${TASK_NAME}_fs15.jsonl"
+for IMP_R in 0.315 0.29
 do
 
 python3 -u run_lm_eval_harness.py \
   --input-path $INPUT_FILE \
-  --output-path logits/impress/30b/${TASK_NAME}_ret_${IMP_R}.jsonl \
+  --output-path logits/impress/6.7b/fs15_${TASK_NAME}_ret_${IMP_R}.jsonl \
   --use-flexgen \
   --model $MODEL_NAME \
   --percent 100 0 100 0 100 0 \
   --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights  \
-  --prompt-len 1300 \
+  --prompt-len 1700 \
   --gen-len 24 \
   --important-ratio=${IMP_R} \
-  --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/30b/lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
+  --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/6.7b/fs15_lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
 
 done 
 
 done
 
-# #############66b###################
-# MODEL_NAME="facebook/opt-66b"
-# # MODEL_NAME="facebook/opt-6.7b"
+############30b###################
+# # MODEL_NAME="facebook/opt-66b"
+# MODEL_NAME="facebook/opt-30b"
 # MODEL_TYPE="opt"
 
-# for TASK_NAME in "openbookqa" "copa" "rte"
-# # for TASK_NAME in "copa"
+# for TASK_NAME in "rte"
 # do
 
-# INPUT_FILE="datasets/${TASK_NAME}_fs10.jsonl"
-# for IMP_R in 0.25 0.3 0.35 0.4 0.45
+# INPUT_FILE="datasets/${TASK_NAME}_fs15.jsonl"
+# for IMP_R in 0.34 0.36 0.44 0.46
 # do
 
 # python3 -u run_lm_eval_harness.py \
 #   --input-path $INPUT_FILE \
-#   --output-path logits/impress/66b/${TASK_NAME}_ret_${IMP_R}.jsonl \
+#   --output-path logits/impress/30b/fs15_${TASK_NAME}_ret_${IMP_R}.jsonl \
+#   --use-flexgen \
+#   --model $MODEL_NAME \
+#   --percent 100 0 100 0 100 0 \
+#   --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights  \
+#   --prompt-len 1700 \
+#   --gen-len 24 \
+#   --important-ratio=${IMP_R} \
+#   --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/30b/fs15_lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
+
+# done 
+
+# done
+
+# # #############66b###################
+# MODEL_NAME="facebook/opt-66b"
+# # MODEL_NAME="facebook/opt-6.7b"
+# MODEL_TYPE="opt"
+
+# # for TASK_NAME in "openbookqa" "copa" "rte"
+# for TASK_NAME in "rte"
+# do
+
+# INPUT_FILE="datasets/${TASK_NAME}_fs15.jsonl"
+# for IMP_R in 0.3 0.35 0.4 0.45 0.5
+# do
+
+# python3 -u run_lm_eval_harness.py \
+#   --input-path $INPUT_FILE \
+#   --output-path logits/impress/66b/fs15_${TASK_NAME}_ret_${IMP_R}.jsonl \
 #   --use-flexgen \
 #   --model $MODEL_NAME \
 #   --percent 40 60 100 0 100 0 \
 #   --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights  \
-#   --prompt-len 1300 \
+#   --prompt-len 1700 \
 #   --gen-len 24 \
 #   --important-ratio=${IMP_R} \
-#   --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/66b/lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
+#   --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/66b/fs15_lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
 
 # done 
 
 # done
 
 
-# # # #############6.7b###################
-# # # MODEL_NAME="facebook/opt-66b"
+##############################
+# # #############6.7b###################
+# # MODEL_NAME="facebook/opt-66b"
 # MODEL_NAME="facebook/opt-6.7b"
 # MODEL_TYPE="opt"
 
-# for TASK_NAME in "openbookqa" "copa" "rte"
-# # for TASK_NAME in "copa"
+# # for TASK_NAME in "openbookqa" "copa" "rte"
+# for TASK_NAME in "copa"
 # do
 
-# INPUT_FILE="datasets/${TASK_NAME}_fs10.jsonl"
-# for IMP_R in 0.25 0.3 0.35 0.4 0.45
+# INPUT_FILE="datasets/${TASK_NAME}_fs20.jsonl"
+# for IMP_R in 0.31 0.32 0.39 0.41 0.48
 # do
 
 # python3 -u run_lm_eval_harness.py \
 #   --input-path $INPUT_FILE \
-#   --output-path logits/impress/6.7b/${TASK_NAME}_ret_${IMP_R}.jsonl \
+#   --output-path logits/impress/6.7b/fs20_${TASK_NAME}_ret_${IMP_R}.jsonl \
 #   --use-flexgen \
 #   --model $MODEL_NAME \
 #   --percent 100 0 100 0 100 0 \
 #   --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights  \
-#   --prompt-len 1300 \
+#   --prompt-len 1700 \
 #   --gen-len 24 \
 #   --important-ratio=${IMP_R} \
-#   --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/6.7b/lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
+#   --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/6.7b/fs20_lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
+
+# done 
+
+# done
+
+# ############30b###################
+# # MODEL_NAME="facebook/opt-66b"
+# MODEL_NAME="facebook/opt-30b"
+# MODEL_TYPE="opt"
+
+# for TASK_NAME in "copa"
+# do
+
+# INPUT_FILE="datasets/${TASK_NAME}_fs20.jsonl"
+# for IMP_R in 0.3 0.35 0.4 0.45 0.5
+# do
+
+# python3 -u run_lm_eval_harness.py \
+#   --input-path $INPUT_FILE \
+#   --output-path logits/impress/30b/fs20_${TASK_NAME}_ret_${IMP_R}.jsonl \
+#   --use-flexgen \
+#   --model $MODEL_NAME \
+#   --percent 100 0 100 0 100 0 \
+#   --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights  \
+#   --prompt-len 1700 \
+#   --gen-len 24 \
+#   --important-ratio=${IMP_R} \
+#   --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/30b/fs20_lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
+
+# done 
+
+# done
+
+# # #############66b###################
+# MODEL_NAME="facebook/opt-66b"
+# # MODEL_NAME="facebook/opt-6.7b"
+# MODEL_TYPE="opt"
+
+# # for TASK_NAME in "openbookqa" "copa" "rte"
+# for TASK_NAME in "copa"
+# do
+
+# INPUT_FILE="datasets/${TASK_NAME}_fs20.jsonl"
+# for IMP_R in 0.3 0.35 0.4 0.45 0.5
+# do
+
+# python3 -u run_lm_eval_harness.py \
+#   --input-path $INPUT_FILE \
+#   --output-path logits/impress/66b/fs20_${TASK_NAME}_ret_${IMP_R}.jsonl \
+#   --use-flexgen \
+#   --model $MODEL_NAME \
+#   --percent 40 60 100 0 100 0 \
+#   --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights  \
+#   --prompt-len 1700 \
+#   --gen-len 24 \
+#   --important-ratio=${IMP_R} \
+#   --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering/impress > ../log/impress/66b/fs20_lm_eval_${TASK_NAME}_ret_${IMP_R}.log 2>&1
 
 # done 
 
