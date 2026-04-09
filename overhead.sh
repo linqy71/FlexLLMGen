8fed8f1b6247a845d465a474578766c522e31fe0
linqy71 <2575769386@qq.com>
2026-04-09 15:48:05 +0800

    testing compaction threshold

--- overhead.sh
+++ overhead.sh
@@ -10,9 +10,27 @@
 # cd ../../
 
 
-# rm /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/kv_store/*
-# K=6
-# L=8
+rm /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/kv_store/*
+K=6
+L=8
+cr=0.8
+python3 -u -m flexllmgen.LSH_opt_dapr --model facebook/opt-30b \
+        --percent 100 0 100 0 100 0 \
+        --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights \
+        --prompt-len 5000 \
+        --gen-len 5 \
+        --strategy query \
+        --merge=True \
+        --overlap=True \
+        --save-res=False \
+        --K=$K \
+        --L=$L \
+        --compaction-threshold=$cr \
+        --input dapr \
+        --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/  > log/overhead/30b_lsh_K${K}L${L}_cr${cr}_dapr.log 2>&1
+echo "done ${cr}"
+
+# cr=0.5
 # python3 -u -m flexllmgen.LSH_opt_dapr --model facebook/opt-30b \
 #         --percent 100 0 100 0 100 0 \
 #         --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights \
@@ -24,27 +42,28 @@
 #         --save-res=False \
 #         --K=$K \
 #         --L=$L \
-#         --compaction-threshold 1 \
+#         --compaction-threshold=$cr \
 #         --input dapr \
-#         --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/  > log/overhead/30b_lsh_K${K}L${L}_cr1_dapr.log 2>&1
-# echo "done dapr"
+#         --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/  > log/overhead/30b_lsh_K${K}L${L}_cr${cr}_dapr.log 2>&1
+# echo "done ${cr}"
+
 ################
 
-rm /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/kv_store/*
-K=6
-L=20
-python3 -u -m flexllmgen.LSH_opt_dapr --model facebook/opt-30b \
-        --percent 60 40 100 0 100 0 \
-        --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights \
-        --prompt-len 8000 \
-        --gen-len 5 \
-        --strategy query \
-        --merge=True \
-        --overlap=True \
-        --save-res=False \
-        --K=$K \
-        --L=$L \
-        --compaction-threshold 1 \
-        --input long \
-        --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/  > log/overhead/30b_lsh_K${K}L${L}_cr1_longbench.log 2>&1
-echo "done long"
+# rm /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/kv_store/*
+# K=6
+# L=20
+# python3 -u -m flexllmgen.LSH_opt_dapr --model facebook/opt-30b \
+#         --percent 60 40 100 0 100 0 \
+#         --path /HOME/nsccgz_zgchen/nsccgz_zgchen_6/HDD_POOL/hyk/opt_weights \
+#         --prompt-len 8000 \
+#         --gen-len 5 \
+#         --strategy query \
+#         --merge=True \
+#         --overlap=True \
+#         --save-res=False \
+#         --K=$K \
+#         --L=$L \
+#         --compaction-threshold 1 \
+#         --input long \
+#         --offload-dir /ssd/nsccgz_zgchen_6/lqy/flexllmgen_offload_dir/gathering_1/  > log/overhead/30b_lsh_K${K}L${L}_cr1_longbench.log 2>&1
+# echo "done long"
